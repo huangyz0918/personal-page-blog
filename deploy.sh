22983c7c882233df401cc01aa60ddf6abc92019c
huangyz0918 <huangyz0918@gmail.com>
2018-11-21 10:15:20 +0800

    added new materials

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,7 @@ echo "========= All Done ========"
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=mickys.me&token=UIrLz1shlCNhoLSx"
 echo " "
 echo "-------- Push Site to Baidu --------"
-
+git checkout sources
+git add . && git commit -m "added new materials"
+git push
+echo "-------- Push to Github --------"
